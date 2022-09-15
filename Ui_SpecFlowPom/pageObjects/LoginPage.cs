c1622e19a6363536041cca4d329fa9cf41803923
John Ruggles <ruggles@catuslabs.info>
2022-09-15 16:33:07 +0100

    Added scenarios for unknown credentials

--- Ui_SpecFlowPom/pageObjects/LoginPage.cs
+++ Ui_SpecFlowPom/pageObjects/LoginPage.cs
@@ -11,6 +11,7 @@ namespace WebAppTestingExample.pageObjects
          * URLs
          *
          */
+
         string loginPageUrl = "https://gearspace.com/board";
 
         /*
@@ -19,6 +20,8 @@ namespace WebAppTestingExample.pageObjects
          *
          */
 
+        string gBulletinMsg = "//*[contains(text(), 'You have entered an invalid username or password.')]";
+        string joinNow = "//*[contains(text(), 'not registered yet? ')]";
         string loginButton = "//input[@type='submit' and @class='userbar-btn login-btn'] ";
         string passwordEntry = "//input[@id='navbar_password']";
         string privacyModal = "//*[@role='dialog' and @aria-modal='true']//*[contains(text(), 'We value your privacy')] ";
@@ -38,7 +41,7 @@ namespace WebAppTestingExample.pageObjects
             driver.Navigate().GoToUrl(loginPageUrl);
 
             // Handle privacy modal if present 
-            if(Selenium.IsElementPresent(driver, By.XPath(privacyModal), 15))
+            if(Selenium.IsElementPresent(driver, By.XPath(privacyModal), 5))
             {
                 driver.FindElement(By.XPath(privacyModalAgree)).Click();
             }
@@ -46,25 +49,32 @@ namespace WebAppTestingExample.pageObjects
 
         internal void EnterUsername(IWebDriver driver, string username)
         {
-            Selenium.FindElement(driver, By.XPath(usernameEntry), 15).SendKeys(username);
+            Selenium.FindElement(driver, By.XPath(usernameEntry), 5).SendKeys(username);
         }
 
         internal void EnterPassword(IWebDriver driver, string password)
         {
-            Selenium.FindElement(driver, By.XPath(passwordEntry), 15).SendKeys(password);
+            Selenium.FindElement(driver, By.XPath(passwordEntry), 5).SendKeys(password);
         }
 
         internal void ClickLoginButton(IWebDriver driver)
         {
-            Selenium.FindElement(driver, By.XPath(loginButton), 15).Click();
+            Selenium.FindElement(driver, By.XPath(loginButton), 5).Click();
         }
 
         internal void ExpectToBeLoggedInAs(IWebDriver driver, string username)
         {
             string thisUsernameLocator = welcomeUsername + "/*[contains(text(), '" + username + "')]";
             
-            Assert.That(Selenium.IsElementPresent(driver, By.XPath(welcome), 15), Is.True);
+            Assert.That(Selenium.IsElementPresent(driver, By.XPath(welcome), 5), Is.True);
             Assert.That(Selenium.IsElementPresent(driver, By.XPath(thisUsernameLocator), 15), Is.True);
+        }
+
+        internal void ExpectToNotBeLoggedIn(IWebDriver driver)
+        {
+            Assert.That(Selenium.IsElementPresent(driver, By.XPath(welcome), 5), Is.False);
+            Assert.That(Selenium.IsElementPresent(driver, By.XPath(joinNow), 5), Is.True);
+            Assert.That(Selenium.IsElementPresent(driver, By.XPath(gBulletinMsg), 5), Is.True);
         }
     }
 }

8f7cc56e866ff1d93c7c7c54d13d8a510d6f6406
John Ruggles <ruggles@catuslabs.info>
2022-09-16 09:24:40 +0100

    Spelling error fix

--- Ui_SpecFlowPom/configs/Config.cs
+++ Ui_SpecFlowPom/configs/Config.cs
@@ -2,7 +2,7 @@
 {
     internal class Config
     {
-        // This isn't an advisable approach, but is used for the simplicity of demonstatio
+        // This isn't an advisable approach, but is used for the simplicity of demonstation
         // See readme for further details, however - register as a user with https://gearspace.com/,
         // validate the membership, then add your username and password values here.  Again,
         // this is far from ideal and credentials, particularly passwords should not be committed.
